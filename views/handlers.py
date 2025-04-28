f446eda64afa48e948cade3f5352aef8972f481f
Alex-Prkd <sadlanay18@gmail.com>
2025-04-28 22:16:19 +0300

    end price page, start create reviews page

--- views/handlers.py
+++ views/handlers.py
@@ -5,6 +5,19 @@ from flask import render_template
 
 def main_page():
     names_img: list = os.listdir("static/img/photos")
-    return render_template('index.html',
-                           images=names_img
-                           )
+    return render_template(
+        'main.html',
+        images=names_img
+    )
+
+
+def price_page():
+    return render_template(
+        "price.html"
+    )
+
+
+def review_page():
+    return render_template(
+        "reviews_page.html"
+    )
--- views/register_handlers.py
+++ views/register_handlers.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
-from views.handlers import main_page
+from views.handlers import main_page, price_page, review_page
 
 
 def pages(app: Flask):
     app.add_url_rule("/", methods=["GET", "POST"], view_func=main_page)
+    app.add_url_rule("/price", methods=["GET", "POST"], view_func=price_page)
+    app.add_url_rule("/reviews", methods=["GET", "POST"], view_func=review_page)
